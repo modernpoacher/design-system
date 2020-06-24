fb7e0bb6fd40182e6c5b88b5bd4321188e33ce2e
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-06-24 17:25:33 +0100

    Babel

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,9 @@ const log = debug('@modernpoacher/design-system')
 
 const {
   env: {
-    DEBUG = '@modernpoacher/design-system',
     NODE_ENV = 'development'
   }
 } = process
-
-debug.enable(DEBUG)
 
 log('`@modernpoacher/design-system` is awake')
 
