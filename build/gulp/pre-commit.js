bcb2025b53628b2d6573a3c519a5133f2b17a587
Jonathan Perry <jonathanperry@modernpoacher.com>
2020-07-07 13:15:56 +0100

    `git status`

--- build/gulp/pre-commit.js
+++ build/gulp/pre-commit.js
@@ -9,13 +9,36 @@ const log = debug('@modernpoacher/design-system:build:gulp:pre-commit')
 log('`@modernpoacher/design-system` is awake')
 
 const PACKAGE = /-+\s+"version":\s"(\d+\.\d+\.\d+)",\s+\++\s+"version":\s"(\d+\.\d+\.\d+)",\s+/s
+
+const HAS_STAGED_CHANGES = /Changes to be committed/s
+
+const NOT_STAGED_CHANGES = /Changes not staged for commit/s
+
 const OPTIONS = {
   maxBuffer: 1024 * 500
 }
 
-const hasPackageVersionChanges = () => (
+const hasPackageVersionChanges = () => ( // eslint-disable-line
   new Promise((resolve, reject) => {
     exec('git diff HEAD origin/master package.json', OPTIONS, (e, v) => (!e) ? resolve(PACKAGE.test(v)) : reject(e))
+  })
+)
+
+const notPackageVersionChanges = () => (
+  new Promise((resolve, reject) => {
+    exec('git diff HEAD origin/master package.json', OPTIONS, (e, v) => (!e) ? resolve(PACKAGE.test(v) !== true) : reject(e))
+  })
+)
+
+const hasStagedChanges = () => (
+  new Promise((resolve, reject) => {
+    exec('git status', OPTIONS, (e, v) => (!e) ? resolve(HAS_STAGED_CHANGES.test(v)) : reject(e))
+  })
+)
+
+const notStagedChanges = () => (
+  new Promise((resolve, reject) => {
+    exec('git status', OPTIONS, (e, v) => (!e) ? resolve(NOT_STAGED_CHANGES.test(v)) : reject(e))
   })
 )
 
@@ -25,20 +48,23 @@ const patchPackageVersion = () => (
   })
 )
 
-const addPackageChanges = () => (
+const addPackageVersionChanges = () => (
   new Promise((resolve, reject) => {
     exec('git add package.json package-lock.json', OPTIONS, (e) => (!e) ? resolve() : reject(e))
   })
 )
 
-export default async () => {
+export default async function preCommit () {
   log('pre-commit')
 
   try {
-    const b = await hasPackageVersionChanges()
-    if (!b) {
-      await patchPackageVersion()
-      await addPackageChanges()
+    if (await notStagedChanges()) return
+
+    if (await hasStagedChanges()) {
+      if (await notPackageVersionChanges()) {
+        await patchPackageVersion()
+        await addPackageVersionChanges()
+      }
     }
   } catch ({ message = 'No error message defined' }) {
     log(message)
